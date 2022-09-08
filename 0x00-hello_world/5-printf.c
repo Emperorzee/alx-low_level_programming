88c376306c41e1f8e429341828afecd883bc1097
Emperorzee <omozee26@gmail.com>
2022-09-08 14:26:56 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main: print a numch of crap
+* 
+* Description: prints stuff
+* the programme should print with proper grammar, but the outcome is a piece of art,
+* Return: 0
+*/
+
+int main(void)
+{
+	printf(with proper grammar, but the outcome is a piece of art,\n);
+	return (0);
+}
