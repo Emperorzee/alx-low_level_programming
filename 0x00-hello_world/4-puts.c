314be8debf2a4b6f73a62e02513520f8610cc69d
Emperorzee <omozee26@gmail.com>
2022-09-08 13:57:12 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+* main - print a bunch of stuff
+*
+* Description: this programme prints Programming is like building a multilingual puzzle
+* Return 0
+*/
+
 int main(void)
 {
-	 puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts( "\"Programming is like building a multilingual puzzle" );
+	return (0);
 }
