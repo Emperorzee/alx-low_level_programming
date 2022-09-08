fbddae7ffe2d5f62e9e46de15fbc639a151e81d9
Emperorzee <omozee26@gmail.com>
2022-09-08 15:03:19 -0700

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - find a bunch of stuff
+*
+*
+* Description: numbers and shit
+* print each number and stuff
+* Return: 0
+*/
+
+int main(void)
+{
+
+	int i;
+	double d;
+	char c;
+
+	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
+	printf("The size of a double is: %lu.\n", (unsigned long)sizeof(d));
+	printf("The size of a c is %lu.\n", (unsigned long)sizeof(c));
+
+	return (0)
+}
