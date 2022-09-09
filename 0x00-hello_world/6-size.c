b06b8e225a9567fdf907b64d59d680696ec7bf27
Emperorzee <omozee26@gmail.com>
2022-09-08 17:59:51 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 
 /**
-* main - nonsense and ingredients
+* main - print a bunch of crap
 *
-* Description: blah blah blahhhhh
-* print a whole bunch of crap
+* Description: printing a bunch of crap
+* in a string priting bits
 * Return: 0
 */
 
 int main(void)
 {
-	int i;
-	double d;
 	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
 
-	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
-	printf("The size of a double is: %lu.\n", (unsigned long)sizeof(d));
-	printf("The size of a char is: %lu.\n", (unsigned long)size0f(c));
-
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli);
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
-
