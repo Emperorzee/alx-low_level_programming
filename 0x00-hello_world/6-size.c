6d08fc40e052e3b29c7dbb0b924a96443b3573e4
Emperorzee <omozee26@gmail.com>
2022-09-08 18:33:01 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,9 +2,10 @@
 
 /**
 * main - prints the size of various types
-* Retrun: 0 if exited properly, non-zero otherwise
+* Description: printing the size of various types
+* printing eeach output
+* Return: 0 if exited properly, non-zero otherwise
 */
-
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
@@ -12,6 +13,5 @@ int main(void)
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
-	return (0);
+	return (0)
 }
-
